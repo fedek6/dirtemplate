00ccce59e588e416defcd6e829b359d49c63b825
Konrad Fedorczyk <contact@realhe.ro>
2021-11-11 19:56:59 +0100

    ✨ Add first working version

--- index.js
+++ index.js
@@ -1,2 +1,43 @@
 #!/usr/bin/env node
-console.log("Hello, here is my first CLI tool")
+const fs = require("fs");
+const path = require("path");
+const { program } = require("commander");
+
+program
+  .version("0.0.1")
+  .argument("<dirname>", "directory name to crate structure")
+  .option("-t, --template <template>", "which template to use", "multimedia")
+  .action((dirname, { template }) => {
+    const dir = path.join("./", dirname);
+    const templatePath = path.join(__dirname, "templates", `${template}.json`);
+    let jsonData;
+
+    try {
+      // Read template
+      if (fs.existsSync(templatePath)) {
+        jsonData = require(templatePath);
+      } else {
+        throw new Error(`There's no ${template} template`);
+      }
+
+      // Create directory
+      if (!fs.existsSync(dir)) {
+        fs.mkdirSync(dir);
+      } else {
+        throw new Error(`Directory ${dir} already exists`);
+      }
+
+      // Create template directories
+      for(let newDir in jsonData) {
+        console.log(`Creating directory: ${newDir}`)
+        const templateDir = path.join(dir, newDir);
+        fs.mkdirSync(templateDir);
+      }
+
+      console.log("That's all!");
+    } catch(err) {
+      console.error("Error: " + err.message);
+    }
+  });
+
+program.parse();
