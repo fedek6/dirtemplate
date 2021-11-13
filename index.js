5ce985ce1c18de980a1c1f51948a3e54f8bd564f
Konrad Fedorczyk <contact@realhe.ro>
2021-11-13 14:23:32 +0100

    Add support for local paths

--- index.js
+++ index.js
@@ -3,21 +3,34 @@ const fs = require("fs");
 const path = require("path");
 const { program } = require("commander");
 
+const genTemplatePath = (template) => {
+  const nameRegex = /^[a-z]+$/;
+
+  if (nameRegex.test(template)) {
+    return path.resolve(__dirname, "templates", `${template}.json`);
+  } else if (template.substr(-5) === ".json") {
+    return path.resolve("./", template);
+  } else {
+    throw new Error("Template file must have a json extension");
+  }
+}
+
 program
   .version("0.0.1")
   .argument("<dirname>", "directory name to crate structure")
   .option("-t, --template <template>", "which template to use", "multimedia")
   .action((dirname, { template }) => {
     const dir = path.join("./", dirname);
-    const templatePath = path.join(__dirname, "templates", `${template}.json`);
     let jsonData;
 
     try {
+      const templatePath =  genTemplatePath(template);
+
       // Read template
       if (fs.existsSync(templatePath)) {
         jsonData = require(templatePath);
       } else {
-        throw new Error(`There's no ${template} template`);
+        throw new Error(`There's no ${templatePath} template`);
       }
 
       // Create directory
@@ -28,14 +41,14 @@ program
       }
 
       // Create template directories
-      for(let newDir in jsonData) {
-        console.log(`Creating directory: ${newDir}`)
+      for (let newDir in jsonData) {
+        console.log(`Creating directory: ${newDir}`);
         const templateDir = path.join(dir, newDir);
         fs.mkdirSync(templateDir);
       }
 
       console.log("That's all!");
-    } catch(err) {
+    } catch (err) {
       console.error("Error: " + err.message);
     }
   });
